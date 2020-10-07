8ef9fde326050ebc0571a7d1d636a3a3043d56fc
Valeriusdev <kakao757@gmail.com>
2020-10-07 00:57:25 -0700

    some changes

--- index.js
+++ index.js
@@ -39,9 +39,26 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = []
+}   
+  
+  
+  Person.prototype.eat = function (someFood){
+    if (this.stomach.length < 10) {
+      return this.stomach.push(someFood) 
+    }
+  };
+  Person.prototype.poop = function(){
+    this.stomach = [];
+  };
+  Person.prototype.toString = function(){
+    return `${this.age}, ${this.name}`
+  };
 
-}
+
 
 /*
   TASK 2
@@ -57,8 +74,14 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+   this.model = model;
+   this.milesPerGallon = milesPerGallon;
+   this.tank = 0;
+   this.odometer = 0;
+}
+Car.prototype.fill = function(gallons){
+  this.tank += gallons
 }
 
 /*
@@ -68,18 +91,31 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
 
+Baby.prototype= Object.create(Person.prototype)
+
+function Baby(name,age,favoriteToy) {
+  this.name = name;
+  this.age = age;
+  this.favoriteToy = favoriteToy;
 }
+
+Baby.prototype.play = function(){
+  return `Plaing with ${this.favoriteToy}`
+};
+
+
+
+
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window binding. If "this" keyword is in global scope the value of it would be the window object.
+  2. Implicit binding. When "this" keyword refers to the declared object.
+  3. New binding. When "this" keyword refers to the specific instance of the created object.
+  4. Explicit binding. When we use call or apply method.
 */
 
 
